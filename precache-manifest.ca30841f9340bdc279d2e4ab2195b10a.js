6ed25337741057274a6a719bd1daf72d557455a0
Kittisak Ardkonghan <kittisak@star-platinum.local>
2020-04-22 14:41:23 +0700

    change

--- precache-manifest.ca30841f9340bdc279d2e4ab2195b10a.js
+++ precache-manifest.ca30841f9340bdc279d2e4ab2195b10a.js
@@ -5,18 +5,18 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "790e83b0dc53e3da718e",
-    "url": "/static/css/main.507cca9e.chunk.css"
+    "url": "static/css/main.507cca9e.chunk.css"
   },
   {
     "revision": "9f4aad9144c89332af3d",
-    "url": "/static/js/2.37e529e9.chunk.js"
+    "url": "static/js/2.37e529e9.chunk.js"
   },
   {
     "revision": "790e83b0dc53e3da718e",
-    "url": "/static/js/main.060331bb.chunk.js"
+    "url": "static/js/main.060331bb.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "static/js/runtime~main.a8a9905a.js"
   }
 ]);
